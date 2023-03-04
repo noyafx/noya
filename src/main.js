db6a9a369d8ee3dde7a4e5cc964a356491ab51b0
や い の <111561391+noyafx@users.noreply.github.com>
2023-03-05 06:26:04 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,4 +10,9 @@ const client = new Client({
 
 client.commands = new Collection();
 
+for (const file of readdirSync("./src/events")) {
+  const event = require(`./events/${file}`);
+  client[event.once ? "once" : "on"](event.name, (...args) => event.execute(...args));
+};
+
 client.login(process.env.DISCORD_TOKEN);
