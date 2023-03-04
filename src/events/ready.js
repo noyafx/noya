11d20f436a4b3c83417ba27a915e063d2b9b6c70
や い の <111561391+noyafx@users.noreply.github.com>
2023-03-04 20:14:23 +0700

    Update and rename client.js to ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,6 @@
 export default {
   once: true,
   async execute(client) {
-    await console.log(`* Connected as ${client.user.tag}`);
+    console.log(`* Connected as ${client.user.tag}`);
   }
 }
