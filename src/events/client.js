8a73de18255483b011015eab2e67fab292a3c54b
や い の <111561391+noyafx@users.noreply.github.com>
2023-03-04 20:11:30 +0700

    Update client.js

--- src/events/client.js
+++ src/events/client.js
@@ -1,11 +1,7 @@
-import Database from "simple-json-db";
-
 export const ready = {
   name: "ready",
   once: true,
   async execute(client) {
-    for (const guild of client.guilds.cache) {
-      guild.database = new Database(`./database/guilds/${guild.id}`);
-    }
+    await console.log(`* Connected as ${client.user.tag}`);
   }
 }
