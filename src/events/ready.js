4a831eeb925d4208f99f51947e072e3a453a7f31
や い の <111561391+noyafx@users.noreply.github.com>
2023-03-05 11:42:46 +0700

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,10 @@
+const Database = require("simple-json-db");
+
 module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
     console.log(`* Connected as ${client.user.tag}`);
+    client.config = new Database(`./database/clients/${client.user.id}/config.json`, { jsonSpaces: 2 });
   }
-}
+};
