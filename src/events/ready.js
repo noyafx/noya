4f10fc5bd5fe92d16b6061b90a760eb5bef37ec9
や い の <111561391+noyafx@users.noreply.github.com>
2023-03-05 11:45:44 +0700

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,7 +4,10 @@ module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
+    await client.application.fetch();
     console.log(`* Connected as ${client.user.tag}`);
-    client.config = new Database(`./database/clients/${client.user.id}/config.json`, { jsonSpaces: 2 });
+    client.config = new Database(`./database/clients/${client.application.id}/config.json`, { jsonSpaces: 2 });
+
+    await client.config.set("lastLogged", new Date());
   }
 };
