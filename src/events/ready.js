c87fb8e189ce06cf3df8ed04ae45bd83390b22ef
や い の <111561391+noyafx@users.noreply.github.com>
2023-03-06 16:22:29 +0700

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,7 @@
+const ytdl = require("ytdl-core");
 const { mkdirp } = require("mkdirp");
 const Database = require("simple-json-db");
+const { createAudioResource, StreamType, createAudioPlayer, joinVoiceChannel } = require("@discordjs/voice");
 
 module.exports = {
   name: "ready",
@@ -13,5 +15,22 @@ module.exports = {
     client.config = new Database(`${configPath}config.json`, { jsonSpaces: 2 });
 
     await client.config.set("lastLogged", new Date());
+    
+    const vc = await client.channels.fetch("1081933594710253578");
+    const connection = joinVoiceChannel({
+      channelId: vc.id,
+      guildId: vc.guildId,
+      adapterCreator: vc.guild.voiceAdapterCreator
+    });
+    const stream = ytdl("https://www.youtube.com/watch?v=jfKfPfyJRdk", {
+      filter: "audioonly"
+    });
+    const resource = createAudioResource(stream, {
+      inputType: StreamType.Opus,
+      inlineVolume: true
+    });
+    const player = createAudioPlayer();
+    player.play(resource);
+    connection.subscribe(player);
   }
 };
