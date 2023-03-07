171582ebf54355921f0ba819a09232f9e81e0a4a
や い の <111561391+noyafx@users.noreply.github.com>
2023-03-07 14:08:48 +0700

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -11,20 +11,20 @@ module.exports = {
         if (message.client.openai.usableAt > Date.now()) return;
         message.client.openai.usableAt = Date.now() + 1000;
 
-        const mainDialogue = "Noya: Hai, nama aku noya";
+        const mainDialogue = `${message.client.user.tag}: Hai, nama aku ${message.client.user.username}`;
         const cache = await message.guild.chatbotCache.get("chats");
         if (!cache.includes(mainDialogue)) cache.push(mainDialogue);
-        cache.push(`User: ${message.content}`);
+        cache.push(`${message.author.tag}: ${message.content}`);
 
         const { data: { choices } } = await message.client.openai.createCompletion({
           model: "text-davinci-003",
-          prompt: `${cache.join("\n")}Noya: `,
+          prompt: `${cache.join("\n")}${message.client.user.username}: `,
           temperature: 0.5,
           max_tokens: 60,
           top_p: 1,
           frequency_penalty: 0.5,
           presence_penalty: 0,
-          stop: ["User:"]
+          stop: [`${message.author.tag}:`]
         });
 
         try {
@@ -32,7 +32,7 @@ module.exports = {
           await message.reply({
             content: result
           });
-          cache.push(`Noya: ${result.replace("\n", "")}`);
+          cache.push(`${message.client.user.tag}: ${result.replace("\n", "")}`);
           await message.guild.chatbotCache.set(cache);
         } catch {}
       }
